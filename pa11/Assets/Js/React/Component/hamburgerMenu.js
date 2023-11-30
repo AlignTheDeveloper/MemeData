b01e07b307ee25b811918bd522e1dbc6fd637b30
Grant <g.rantolson02@gmail.com>
2023-11-29 23:13:34 -0700

    Added so much stuff it is crazy
    yuh

---
+++ pa11/Assets/Js/React/Component/hamburgerMenu.js
@@ -0,0 +1,52 @@
+class HamburgerMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    closeMenu(e) {
+        if (this.node && !this.node.contains(e.target)) {
+            this.setState({ isOpen: false });
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.closeMenu);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.closeMenu);
+    }
+
+    render() {
+        return (
+            <div ref={node => { this.node = node; }}>
+                <div id="hamburger" onClick={this.toggleMenu}>
+                    <h2><i class="fa-solid fa-bars"></i></h2>
+                    <h4>menu</h4>
+                </div>
+                {this.state.isOpen && (
+                    <nav id="navMenu">
+                        <a href="/MemeDataTable.html" alt="Data Table">
+                            <h6>DataTable</h6>
+                        </a>
+                        <a href="/graph1.html" alt="Graph 1">
+                            <h6>Graph1</h6>
+                        </a>
+                        <a href="/graph2.html" alt="Graph 2">
+                            <h6>Graph2</h6>
+                        </a>
+                    </nav>
+                )}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<HamburgerMenu />, document.getElementById('hamburgerMenuContainer'));
