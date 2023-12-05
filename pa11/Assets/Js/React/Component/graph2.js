10dc37d32cca11f82c25a5f2d7be2e7c7573ce8a
Grant <g.rantolson02@gmail.com>
2023-12-05 14:46:16 -0700

    added graph 2
    YUR

--- pa11/Assets/Js/React/Component/graph2.js
+++ pa11/Assets/Js/React/Component/graph2.js
@@ -1,14 +1,14 @@
 class Graph2 extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        originalData: [],
-        wordData: []
-      };
+    constructor (props) {
+        super(props);
+        this.state = {
+            originalData: [],
+            scatterData: []
+        };
     }
-  
+
     componentDidMount() {
-      fetch("https://raw.githubusercontent.com/AlignTheDeveloper/MemeData/main/memeData.json")
+        fetch("https://raw.githubusercontent.com/AlignTheDeveloper/MemeData/main/memeData.json")
         .then(response => {
           if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.status}`);
@@ -17,80 +17,84 @@ class Graph2 extends React.Component {
         })
         .then(jsonData => {
           console.log("Fetched data:", jsonData);
-          // Process the data to get word counts
-          let wordCounts = {};
-          jsonData.forEach(entry => {
-            let memeStyle = entry["Which style of meme have you encountered the most?"];
-            if (memeStyle) {
-              wordCounts[memeStyle] = (wordCounts[memeStyle] || 0) + 1;
-            }
-          });
-          let wordData = Object.keys(wordCounts).map(key => {
-            return { text: key, size: wordCounts[key] };
-          });
-          this.setState({ originalData: jsonData, wordData: wordData });
+        
+          let scatterData = jsonData.map(entry => ({
+              timeStamp: entry["Timestamp"],
+              amusingMeme: entry["Which style of meme do you find the most amusing or interesting?"]
+          }));
+
+          this.setState({ originalData: jsonData, scatterData: scatterData });
         })
-        .catch(error => {
-          console.error("Error fetching data:", error);
-        });
+        .catch(error => console.error("Error fetching data:", error));
     }
-  
+
     componentDidUpdate() {
-      this.createWordCloud();
+        this.createScatter();
     }
-  
-    createWordCloud() {
-        const { wordData } = this.state;
-        if (wordData.length === 0) return;
-      
+
+    createScatter() {
+        const { scatterData } = this.state;
+        if (scatterData.length === 0) return;
+    
         d3.select("#graph2").select("svg").remove();
-      
+    
         const container = d3.select("#graph2");
         const containerWidth = container.node().getBoundingClientRect().width;
-        const containerHeight = Math.max(containerWidth, 450);
-      
-        const svg = container.append("svg")
-          .attr("preserveAspectRatio", "xMidYMid meet")
-          .attr("viewBox", `0 0 ${containerWidth} ${containerHeight}`)
-          .append("g")
-          .attr("transform", `translate(${containerWidth / 2},${containerHeight / 2})`);
-      
-        const layout = d3.layout.cloud()
-          .size([containerWidth, containerHeight])
-          .words(wordData.map(d => ({ text: d.text, size: d.size * 10 }))) // Adjust size scaling as needed
-          .padding(10)
-          .fontSize(d => d.size)
-          .on("end", draw);
-        layout.start();
-      
-        function draw(words) {
-          svg.selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", d => `${d.size}px`)
-            .style("fill", "#172a43")
-            .attr("text-anchor", "middle")
-            .style("font-family", "Impact")
-            .attr("transform", d => `translate(${d.x},${d.y})rotate(${d.rotate})`)
-            .text(d => d.text)
+        const aspectRatio = 3 / 4;
+        const containerHeight = Math.min(containerWidth * aspectRatio, window.innerHeight);
+    
+        // Increased left margin to accommodate y-axis labels
+        const margin = {top: 10, right: 50, bottom: 50, left: 250}; 
+        const width = containerWidth - margin.left - margin.right;
+        const height = containerHeight - margin.top - margin.bottom;
 
-        }
-      }
-      
-      
-      
-      
-  
-    render() {
-      return (
-        <React.Fragment>
-          <div id="graph2"></div>
-        </React.Fragment>
-      );
+        scatterData.forEach(d => {
+            d.timeStampNumeric = new Date(d.timeStamp).getTime(); // Example conversion
+        });
+    
+        const svg = container.append("svg")
+          .attr("width", containerWidth)
+          .attr("height", containerHeight)
+          .append("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        
+    
+        // X axis
+        var x = d3.scaleLinear()
+        .domain(d3.extent(scatterData, d => d.timeStampNumeric)) // Use extent to get min and max
+        .range([0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d/%Y "))); // Format ticks as dates
+    
+        // Y axis 
+        var y = d3.scaleBand()
+          .range([height, 0])
+          .domain(scatterData.map(d => d.amusingMeme))
+        svg.append("g").call(d3.axisLeft(y));
+    
+        // Add dots
+        svg.selectAll("circle")
+          .data(scatterData)
+          .enter()
+          .append("circle")
+            .attr("cx", function (d) { return x(d.timeStampNumeric); })
+            .attr("cy", function (d) { return y(d.amusingMeme) + y.bandwidth() / 2; }) // Center the dot in the band
+            .attr("r", 5)
+            .style("fill", "#172a43");
     }
-  }
-  
-  const container = document.getElementById("graph2");
-  const root = ReactDOM.createRoot(container);
-  root.render(<Graph2 />);
-  
+    
+
+    render() {
+        return (
+            <React.Fragment>
+                <div id="graph2"></div>
+            </React.Fragment>
+        );
+    }
+}
+
+const container = document.getElementById("graph2");
+const root = ReactDOM.createRoot(container);
+root.render(<Graph2 />);
